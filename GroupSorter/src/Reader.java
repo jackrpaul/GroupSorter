10ee2536bd6f3ae591635bc43304e894f53467a5
Jack Paul <jackrpaul13@gmail.com>
2017-11-28 14:38:09 -0700

    made student class

--- GroupSorter/src/Reader.java
+++ GroupSorter/src/Reader.java
@@ -1,9 +1,12 @@
+import java.util.*;
+import java.io.*;
 
 public class Reader
 	{
 
-		public static void main(String[] args)
+		public static void main(String[] args) throws IOException
 			{
+			Scanner file = new Scanner(new File("roster.txt"));
 			
 			}
 
