1108bf5ecb24a4d75d14300566ce6ac8565f97d1
Collin McDade <Collin.mcdade@mullenmail.com>
2017-12-01 14:49:10 -0700

    tester

--- GroupSorter/src/Sorter.java
+++ GroupSorter/src/Sorter.java
@@ -27,31 +27,6 @@ public class Sorter
 							}
 					}
 				lines.add(line.substring(space2+1) + line.substring(space1+1,space2) + line.substring(0,space1));
-				Collections.sort(lines);
-				System.out.println("How many groups do you want?");
-				int groups = inp.nextInt();
-				int temp = groups;
-				for(int x=0; x<lines.size(); x++)
-					{
-						String temp2 = lines.get(x);
-						lines.remove(x);
-						lines.add(x, temp2 + temp);
-						temp--;
-						if(temp==0)
-							{
-								temp=groups;
-							}
-					}
-				for(int x=0; x<lines.size(); x++)
-					{
-						String temp3 = lines.get(x);
-						lines.remove(x);
-						lines.add(x, temp3.substring(4));
-					}
-				for(int x=0; x<lines.size(); x++)
-					{
-						System.out.println(lines.get(x));
-					}
 			}
 
 	}
